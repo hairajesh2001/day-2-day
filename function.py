2a9eb131546b5ff84432057d03eee947e678542e
nrajesh_codes <n_rajeshdit@yahoo.com>
2024-09-15 10:04:15 +0800

    function

--- function.py
+++ function.py
@@ -18,3 +18,17 @@ def product_of_two_numbers(a,b):
     return product
  
 print(product_of_two_numbers(2,3))  # prints 6
+
+
+def calculate_sum_of_three_numbers(a, b, c):
+    sum = a + b + c
+    return sum
+
+print(calculate_sum_of_three_numbers(1,4,5))
+
+
+def calculate_cube(a):
+    cube=a*a*a
+    return cube
+
+print(calculate_cube(5))
