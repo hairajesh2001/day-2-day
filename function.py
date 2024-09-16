2a87921aa0045e32420329f3014bdbb3f5fd9454
nrajesh_codes <n_rajeshdit@yahoo.com>
2024-09-16 17:18:23 +0800

    added committed

--- function.py
+++ function.py
@@ -32,3 +32,10 @@ def calculate_cube(a):
     return cube
 
 print(calculate_cube(5))
+
+
+
+def average_of_five_numbers(a,b,c,d,e):
+    average=(a+b+c+d+e)/5
+    return average
+print(average_of_five_numbers(5, 10, 15, 20, 25))
